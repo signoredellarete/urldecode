ce716db535408391382e5ee3a349a3a4cc5b85da
Fabrizio Verde <signoredellarete@gmail.com>
2024-04-09 14:17:36 +0200

    src

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# urldecode
+# urlencode
 Linux urldecode for command line, written in Rust.
+
+## Release Compilation to reduce executable size
+```
+RUSTFLAGS='-C strip=symbols' cargo build --release
+```
+
+## How to compile on Alpine linux
+
+apk add gcc
+apk add cc
+apk add build-base
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use clap::Parser;
+use clap_stdin::MaybeStdin;
+use url::form_urlencoded::{byte_serialize, parse};
+
+#[derive(Debug, Parser)]
+struct Args {
+    //#[clap(default_value = "-")]
+    value: MaybeStdin<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+    let decoded: String = parse(args.value.as_bytes())
+        .map(|(key, val)| [key, val].concat())
+        .collect();
+    println!("{}", decoded);
+}
+
+
+
+
+
+
